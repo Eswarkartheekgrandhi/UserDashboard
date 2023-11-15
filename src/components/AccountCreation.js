4670c145f0dac2f36069f54175ded290a701b5c5
Eswarkartheekgrandhi <20pa1a0554@vishnu.edu.in>
2023-11-15 13:23:55 +0530

    firebase connected

--- src/components/AccountCreation.js
+++ src/components/AccountCreation.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import HttpsOutlinedIcon from "@mui/icons-material/HttpsOutlined";
 import "../styles/AccoutCreation.css";
 import logo from "../logo.svg";
+import { auth, createUserWithEmailAndPassword } from "../firebase";
 
 const AccountCreation = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleCreateAccount = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        `${username}@gmail.com`,
+        password
+      );
+
+      console.log("Account created:", userCredential.user);
+    } catch (error) {
+      console.error("Error creating account:", error.message);
+    }
+  };
+
   return (
     <div className="signUp-container">
       <div className="signUp-form">
         <img className="logo" src={logo} alt="Logo-here" />
         <div className="holder">
           <PersonOutlineOutlinedIcon className="user-icon" />
-          <input type="text" placeholder="USERNAME" className="input-field" />
+          <input
+            type="text"
+            placeholder="USERNAME"
+            className="input-field"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className="holder">
           <HttpsOutlinedIcon className="user-icon" />
@@ -19,9 +43,13 @@ const AccountCreation = () => {
             type="password"
             placeholder="PASSWORD"
             className="input-field"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button className="input-field">Create Account</button>
+        <button className="input-field" onClick={handleCreateAccount}>
+          Create Account
+        </button>
       </div>
     </div>
   );
